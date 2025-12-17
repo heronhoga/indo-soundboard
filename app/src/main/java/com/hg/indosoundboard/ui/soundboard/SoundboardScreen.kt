e61d372999ac4d49e73e2f2efbc2b5c2ffaac0ab
heronhoga <nozzeravixer@gmail.com>
2025-12-17 16:09:03 +0700

    fix:fix top bar alignment

--- app/src/main/java/com/hg/indosoundboard/ui/soundboard/SoundboardScreen.kt
+++ app/src/main/java/com/hg/indosoundboard/ui/soundboard/SoundboardScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,15 +25,16 @@ fun SoundboardScreen() {
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("Indonesian Core Sound Board") }
+            CenterAlignedTopAppBar(
+                title = { Text("Indonesian Code Sound Board") }
             )
         }
-    ) {
-        padding ->
+    ) { padding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier.padding(padding).padding(16.dp),
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
@@ -41,4 +43,5 @@ fun SoundboardScreen() {
             }
         }
     }
+
 }
